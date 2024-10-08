00c8e3d175607d874b217d4d25c63c06a79fcb86
Risfat97 <tamsir97@gmail.com>
2024-10-08 20:16:09 +0200

    Assignment - Implement Customer Get Operations by sirtaf

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testlistAllCustomers() {
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyCustomerList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testGetCustomerById() {
+        Customer customer = customerRepository.findAll().get(0);
+        CustomerDTO dto = customerController.getCustomerById(customer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+}
